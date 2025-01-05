37604e2b902f7e0105c1ba2a42f4ea792a04ad60
Suraj-Encoding <dalvisuraj256@gmail.com>
2025-01-05 21:39:00 +0530

    enhance success response handling: modify SendSuccess to accept string and add SendPayload function for structured payload responses

--- utils/success.go
+++ utils/success.go
@@ -7,17 +7,35 @@ import (
 )
 
 // # Send Success Response
-func SendSuccess(w http.ResponseWriter, msg interface{}) {
+func SendSuccess(w http.ResponseWriter, msg string) {
 	// # Set Headers for Success Response
 	w.WriteHeader(http.StatusOK) // # 200
 
-	// # Create New Encoder for Response Body
+	// # Create New Encoder for Success Body
 	encoder := json.NewEncoder(w)
 
 	// # Create Success Response Object
 	res := &schema.Success{
 		Success: true,
 		Message: msg,
+	}
+
+	// # Encode Response to Writer
+	encoder.Encode(res)
+}
+
+// # Send Payload Response
+func SendPayload(w http.ResponseWriter, data interface{}) {
+	// # Set Headers for Payload Response
+	w.WriteHeader(http.StatusOK) // # 200
+
+	// # Create New Encoder for Payload Body
+	encoder := json.NewEncoder(w)
+
+	// # Create Payload Response Object
+	res := &schema.Payload{
+		Success: true,
+		Payload: data,
 	}
 
 	// # Encode Response to Writer
