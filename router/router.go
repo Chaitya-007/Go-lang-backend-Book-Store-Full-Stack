55cb3879151224ef402607a21f7e3ff50c92722a
Suraj-Encoding <dalvisuraj256@gmail.com>
2025-01-05 20:13:26 +0530

    added standard api routing structure

--- router/router.go
+++ router/router.go
@@ -6,24 +6,39 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// # Router Function
+// # Router
 func Router() *mux.Router {
 	// # Create a new router object
 	router := mux.NewRouter()
 
 	// # Root Route
-	router.HandleFunc("/", api.RootRoute).Methods("GET") // # Get the server message
+	router.HandleFunc("/", api.RootRoute).Methods("GET") // # Get server welcome message
 
-	// # Create a subrouter for books routes
-	books := router.PathPrefix("/books").Subrouter()
+	// # API Base Path
+	BasePath := "/api/v1"
 
-	// # Add other routes to the subrouter
-	books.HandleFunc("", api.CreateBook).Methods("POST")        // # Create a new book
-	books.HandleFunc("", api.GetBooks).Methods("GET")           // # Get all books
-	books.HandleFunc("", api.DeleteBooks).Methods("DELETE")     // # Delete all books
-	books.HandleFunc("/{id}", api.GetBook).Methods("GET")       // # Get a single book
-	books.HandleFunc("/{id}", api.UpdateBook).Methods("PUT")    // # Update a book
-	books.HandleFunc("/{id}", api.DeleteBook).Methods("DELETE") // # Delete a book
+	// # API Book Resource Path
+	BookResPath := "/book"
+
+	// # API Books Resource Path
+	BooksResPath := "/books"
+
+	// # API Endpoints Slice
+	endpoints := []string{BasePath + BookResPath, BasePath + BooksResPath}
+
+	// # Create a subrouter for the book route
+	book := router.PathPrefix(endpoints[0]).Subrouter()
+
+	// # Create a subrouter for the books route
+	books := router.PathPrefix(endpoints[1]).Subrouter()
+
+	// # Add routes to the subrouter
+	book.HandleFunc("", api.CreateBook).Methods("POST")        // # Create a new book
+	books.HandleFunc("", api.GetBooks).Methods("GET")          // # Get all books
+	books.HandleFunc("", api.DeleteBooks).Methods("DELETE")    // # Delete all books
+	book.HandleFunc("/{id}", api.GetBook).Methods("GET")       // # Get a single book
+	book.HandleFunc("/{id}", api.UpdateBook).Methods("PUT")    // # Update a book
+	book.HandleFunc("/{id}", api.DeleteBook).Methods("DELETE") // # Delete a book
 
 	// # Return Router Object
 	return router
